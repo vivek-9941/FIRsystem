88da2700a86933d70a087218cb1070402bda9459
shrihari7396 <shriharik121@gmail.com>
2025-04-08 18:12:43 +0530

    Update classes

--- src/main/java/org/fir/firsystem/Controller/GlobalExceptionController.java
+++ src/main/java/org/fir/firsystem/Controller/GlobalExceptionController.java
@@ -1,4 +1,18 @@
 package org.fir.firsystem.Controller;
 
+
+import org.fir.firsystem.exception.ComplaintNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionController {
+
+    @ExceptionHandler(ComplaintNotFoundException.class)
+    public ResponseEntity<?> complaintNotFound(ComplaintNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+    
 }
