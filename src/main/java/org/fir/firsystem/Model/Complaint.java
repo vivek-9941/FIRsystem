8b5f3839c5b5e13a00e21905ad773d0b44b582c9
shrihari7396 <shriharik121@gmail.com>
2025-04-08 21:32:46 +0530

    Update classes

--- src/main/java/org/fir/firsystem/Model/Complaint.java
+++ src/main/java/org/fir/firsystem/Model/Complaint.java
@@ -4,21 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor
 public class Complaint {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-
     private ComplaintStatus status;
 }
