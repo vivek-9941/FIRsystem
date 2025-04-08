92fda3a53c49a06fc317259adc38177a5b1859e4
Lenovo <vivekmane9860@gmail.com>
2025-04-08 17:52:54 +0530

    Initial commit

---
+++ src/main/java/org/fir/firsystem/FiRsystemApplication.java
@@ -0,0 +1,13 @@
+package org.fir.firsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiRsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiRsystemApplication.class, args);
+	}
+
+}
