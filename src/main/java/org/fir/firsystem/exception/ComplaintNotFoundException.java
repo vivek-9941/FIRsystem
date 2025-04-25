a8816a82072560a61395d49adf19807139c7ff6d
shrihari7396 <shriharik121@gmail.com>
2025-04-25 17:48:16 +0530

    Implemented PoliceController.java with Pagenation

--- src/main/java/org/fir/firsystem/exception/ComplaintNotFoundException.java
+++ src/main/java/org/fir/firsystem/exception/ComplaintNotFoundException.java
@@ -1,6 +1,6 @@
 package org.fir.firsystem.exception;
 
-public class ComplaintNotFoundException extends Exception {
+public class ComplaintNotFoundException extends RuntimeException {
     public ComplaintNotFoundException(String message) {
         super(message);
     }
